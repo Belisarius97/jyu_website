aaa2dbb65d226a1b42a8a23d9d8dbd4659d1bdc2
Jonathon Yu <belisariuscelsus@gmail.com>
2017-08-22 23:02:25 +0000

    500px version, going to go back to local

--- src/Components/Photography/Photography.js
+++ src/Components/Photography/Photography.js
@@ -1,13 +1,126 @@
 import React from 'react';
+import PhotoGallery from '../Gallery/PhotoGallery.js';
+import _500px from '../../Scripts/500px.js';
 
-const Photography = () => (
-  <photography className ="transition-block">
-    <div>
-      <p>
-        Under construction; check back soon!
-      </p>
-    </div>
-  </photography>
-)
+
+class Photography extends React.Component {
+  constructor(props) {
+    super(props);
+        
+    this.state = {
+      photos: null,
+      lightboxOn: false,
+      width: 0,
+      //url?
+    }
+    
+    this.updateWidth = this.updateWidth.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.loadPhotos = this.loadPhotos.bind(this);
+  }
+  
+  componentDidMount() {
+    this.updateWidth();
+    window.addEventListener('resize', this.updateWidth);
+    this.loadPhotos();
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWidth);
+  }
+  
+  updateWidth() {
+    this.setState({width: window.InnerWidth});
+  }
+  
+  openLightbox() {
+    this.setState({
+      lightboxOn: true,
+    })
+  }
+  
+  closeLightbox() {
+    this.setState({
+      lightboxOn: false,
+    })
+  }
+  
+  loadPhotos() {
+    //var PhotosSrc = '../../Assets/Photos';
+    //fetch photos from 500px's api
+    _500px.init({
+      sdk_key: 'cf5933b771db58244be8922b9780653d8d8e6bcc',
+    });
+    
+    _500px.api('/users', (response) => {
+      let user = response.data.user;
+      
+      _500px.api('users/:user_id/galleries/:id/items', (response) => {
+        if(response.success) {
+          let Photos = response.photos.map((photo) => {
+            return {
+              src: photo.image_url, //url, deprecated? check api (images??)
+              width: photo.width, //exif
+              height: photo.height,
+              alt: photo.name, //exif, title/name?
+              
+            };
+          });
+          this.setState({
+            photos: Photos,
+          });
+        }
+      });
+      
+    });
+    
+    const authParams = {
+      consumerKey: 'uyaQYtwvIEZfgA9h5WrlMaDGXZG6A5zC3KEW39FS',
+      userID: "placeholder",
+      collectionID: 'placeholder',
+      
+    }
+    
+    var PhotoSrc = ""
+    $.ajax({
+      url: PhotoSrc,
+      success: (data) => {
+        let Photos = data.map((elem) => {
+          return {
+            src: elem.url, //url, ../../Assets/Photos/whateverthisoneis
+            width: elem.width, //exif
+            height: elem.height,
+            alt: elem.name, //exif, title/name?
+            
+          };
+        });        
+      }
+    });
+
+    this.setState({
+      photos: Photos,
+    });
+  }
+
+  
+  //make the gallery modestly responsive
+  scaleGallery() {
+    let cols = 1;
+    if(this.width > 480) { cols = 2; }
+    if(this.width > 1024) { cols = 3; }
+    if(this.width > 1920) { cols = 4; }
+    return <PhotoGallery photos={this.photos} cols={cols} onClickPhoto={this.openLightbox} />
+  }
+  
+  render() {
+    this.loadPhotos();
+    return (
+      <Photography>
+        {this.scaleGallery}
+      </Photography>
+    );
+  }
+}
 
 export default Photography;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
     document.getElementById('root')
-    );
+  );
 registerServiceWorker();
 
     // <div>
